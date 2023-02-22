20f63e827029c0a45123a01087564e301074468d
vrajrana1 <125870428+vrajrana1@users.noreply.github.com>
2023-02-22 18:11:50 +0530

    Add files via upload

---
+++ Task-7/4_Regular Expression/form2.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+  <title>Regular Expression Example</title>
+</head>
+<body>
+  <h1>Regular Expression Example</h1>
+  <form method="post">
+    <label>String: <input type="text" name="string"></label><br>
+    <input type="submit" name="submit" value="Remove">
+  </form>
+  
+  <?php
+  if (isset($_POST['submit'])) {
+    $string = $_POST['string'];
+    $numeric_only = preg_replace("/[^0-9]/", "", $string);
+    $alpha_space_only = preg_replace("/[^a-zA-Z\s]/", "", $string);
+
+    echo "<p>Original string: $string</p>";
+    echo "<p>After removing non-numeric characters: $numeric_only</p>";
+    echo "<p>After removing non-alphabet and non-space characters: $alpha_space_only</p>";
+  }
+  ?>
+</body>
+</html>
